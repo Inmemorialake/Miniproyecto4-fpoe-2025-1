f03029be80266c10dfee4c09fdfb3fd8552788c9
Inmemorialake <inmemorialake2@gmail.com>
2025-07-31 01:57:19 -0500

    Write the view comments

--- src/main/java/org/example/eiscuno/view/ClickToContinueStage.java
+++ src/main/java/org/example/eiscuno/view/ClickToContinueStage.java
@@ -1,5 +1,6 @@
 package org.example.eiscuno.view;
 
+// Imports
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -8,15 +9,26 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * ClickToContinueStage is a JavaFX Stage that displays a "Click to Continue" screen.
+ * It is used to pause the game and wait for user interaction before proceeding.
+ */
 public class ClickToContinueStage extends Stage {
+    /**
+     * Constructor for ClickToContinueStage.
+     * It loads the FXML file and sets up the stage with the scene.
+     *
+     * @throws IOException if there is an error loading the FXML file.
+     */
     public ClickToContinueStage() throws IOException {
+        // Loading the FXML file for the ClickToContinueStage
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/eiscuno/click-to-continue.fxml"));
         Parent root;
         try {
             root = loader.load();
         } catch (IOException e) {
             // Re-throwing the caught IOException
-            throw new IOException("Error while loading FXML file", e);
+            throw new IOException("Error while loading FXML file", e); // This will allow the caller to handle the exception
         }
         Scene scene = new Scene(root);
         // Configuring the stage
@@ -26,12 +38,23 @@ public class ClickToContinueStage extends Stage {
         show(); // Displays the stage
     }
 
+    /**
+     * Returns an instance of ClickToContinueStage.
+     * This method implements the Singleton pattern to ensure only one instance exists.
+     *
+     * @return The singleton instance of ClickToContinueStage.
+     * @throws IOException if there is an error loading the FXML file.
+     */
     public static ClickToContinueStage getInstance() throws IOException {
         return ClickToContinueStage.ClickToContinueHodler.INSTANCE != null ?
                 ClickToContinueStage.ClickToContinueHodler.INSTANCE :
                 (ClickToContinueStage.ClickToContinueHodler.INSTANCE = new ClickToContinueStage());
     }
 
+    /**
+     * Closes the ClickToContinueStage.
+     * This method hides the stage and sets the instance to null.
+     */
     private static class ClickToContinueHodler {
         private static ClickToContinueStage INSTANCE;
     }
--- src/main/java/org/example/eiscuno/view/StartStage.java
+++ src/main/java/org/example/eiscuno/view/StartStage.java
@@ -1,6 +1,6 @@
 package org.example.eiscuno.view;
 
-import javafx.fxml.FXML;
+// Imports
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -8,7 +8,17 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * StartStage is a JavaFX Stage that represents the initial screen of the Uno game.
+ * It is used to display the starting interface and wait for user interaction to begin the game.
+ */
 public class StartStage extends Stage {
+    /**
+     * Constructor for StartStage.
+     * It loads the FXML file and sets up the stage with the scene.
+     *
+     * @throws IOException if there is an error loading the FXML file.
+     */
     public StartStage() throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/eiscuno/start-stage.fxml"));
         Parent root;
@@ -26,12 +36,23 @@ public class StartStage extends Stage {
         show(); // Displays the stage
     }
 
+    /**
+     * Returns an instance of StartStage.
+     * This method implements the Singleton pattern to ensure only one instance exists.
+     *
+     * @return The singleton instance of StartStage.
+     * @throws IOException if there is an error loading the FXML file.
+     */
     public static StartStage getInstance() throws IOException {
         return StartStage.StartHolder.INSTANCE != null ?
                 StartStage.StartHolder.INSTANCE :
                 (StartStage.StartHolder.INSTANCE = new StartStage());
     }
 
+    /**
+     * Closes the StartStage.
+     * This method hides the stage and sets the instance to null.
+     */
     private static class StartHolder {
         private static StartStage INSTANCE;
     }
